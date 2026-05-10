ee8967a0eb02cea57436a6ce40cd4ae3447191ee
MennaAllahZakaria <mennazakaria2003@gmail.com>
2026-05-10 18:59:05 +0300

    feat: reorganize middleware usage in product routes for improved security and clarity

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -20,17 +20,17 @@ const { protect, allowedTo } = require("../middleware/authMiddleware");
 const {uploadImages, attachUploadedLinks} = require("../middleware/uploadImageMiddleware");
 const router = express.Router();
 
+
+router.post("/",protect, allowedTo("admin"), uploadImages , attachUploadedLinks ,createProductValidator, createProduct);
+
 router.get ("/", getProducts);
+
 router.get ("/:id" ,idValidator, getProduct);
 
-router.use(protect );
+router.put("/:id" ,protect, allowedTo("admin") , uploadImages , attachUploadedLinks,updateProductValidator , updateProduct);
 
-router.post("/", allowedTo("admin"), uploadImages , attachUploadedLinks ,createProductValidator, createProduct);
+router.patch("/:id/images",protect ,allowedTo("admin"), uploadImages , attachUploadedLinks, idValidator, addImageToProduct);
 
-router.put("/:id" , allowedTo("admin") , uploadImages , attachUploadedLinks,updateProductValidator , updateProduct);
-
-router.patch("/:id/images" ,allowedTo("admin"), uploadImages , attachUploadedLinks, idValidator, addImageToProduct);
-
-router.delete("/:id" ,allowedTo("admin"), idValidator , deleteProduct);
+router.delete("/:id" ,protect,allowedTo("admin"), idValidator , deleteProduct);
 
 module.exports = router;
