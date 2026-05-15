a79a89fc95b8e0a84f530de0169f833df94aa1f5
MennaAllahZakaria <mennazakaria2003@gmail.com>
2026-05-16 00:12:01 +0300

    fix: remove duplicate middleware usage in cart routes

--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -15,9 +15,9 @@ const {
 const { protect, allowedTo ,identifyUser} = require("../middleware/authMiddleware");
 const router = express.Router();
 
-router.post("/add", identifyUser,  identifyUser, addToCart);
-router.delete("/remove/:productId", identifyUser,  identifyUser, removeFromCart);
-router.patch("/decrease/:productId", identifyUser,  identifyUser, decreaseCartItem);
-router.get("/my-cart", identifyUser,  getMyCart);
+router.post("/add", identifyUser, addToCart);
+router.delete("/remove/:productId", identifyUser, removeFromCart);
+router.patch("/decrease/:productId", identifyUser, decreaseCartItem);
+router.get("/my-cart", identifyUser, getMyCart);
 
 module.exports = router;
