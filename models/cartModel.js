7bfb84c7f8b033ad8fdfdb72c141382781162d49
MennaAllahZakaria <mennazakaria2003@gmail.com>
2026-05-15 23:55:56 +0300

    feat: remove unique constraint from user field in cart schema

--- models/cartModel.js
+++ models/cartModel.js
@@ -4,7 +4,6 @@ const cartSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
-        unique: true, // Each user has one cart
     },
     guestId: {
         type: String,
